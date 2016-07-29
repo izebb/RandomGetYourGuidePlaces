2ddf030e9ba0df137fd702dbc68aff22fe1af7f5
izebb <you@example.com>
2016-07-29 01:47:18 +0100

    views

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,11 @@ var customerReducers =  require('./reducers/customerReducers');
 var customerAction =  require('./actions/customer');
 
 var customer  = new EventEmitter(customerReducers);
+var views = new View('root');
 
 customer.on('change', function(state){
 	//Change View
+
 });
 
 customerAction.fetchCustomer(customer);
